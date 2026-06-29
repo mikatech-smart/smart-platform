55cc38681759974c415e933ee368a70c53a39ea4
mikatech-smart <adilson@mikatech.com.br>
2026-06-29 18:31:05 -0300

    Add event listeners for Wi-Fi and WhatsApp actions

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelector(".wifi").onclick=()=>{
+
+alert("Configuração do Wi-Fi em breve.");
+
+}
+
+document.querySelector(".whatsapp").onclick=()=>{
+
+window.open("https://wa.me/5515999999999");
+
+}
+
+document.querySelector(".review").onclick=()=>{
+
+window.open("https://google.com");
+
+}
